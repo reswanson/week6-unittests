5968af6e4b211b6235b8b38a011af52d8d60b35e
reswanson <swansonr3@csp.edu>
2020-06-18 22:45:43 -0500

    Adding tests for each method, (except for stack and stack(int)

--- src/test/java/edu/csc422/week6/StackTest.java
+++ src/test/java/edu/csc422/week6/StackTest.java
@@ -5,22 +5,8 @@ import static org.junit.Assert.*;
 
 
 public class StackTest {
+
 	
-	@Test
-	public void testSize() {
-        System.out.println("testSize");
-        int expResult = 2;
-        
-		Stack instance = new Stack();
-		instance.push(1);
-		instance.push(1);
-		int itssize=instance.size();
-        assertEquals(1, itssize);
-
-
-        //assertEquals(1, instance.size());
-	}
-
 	/*
 	@Test
 	public void testStack() {
@@ -31,35 +17,86 @@ public class StackTest {
 	public void testStackInt() {
 		fail("Not yet implemented");
 	}
+	*/
+	
+	
+	@Test
+	public void testSize() {
+		System.out.println("testSize");
+
+		Stack instance = new Stack();
+		try {
+			// To test failure, comment out one of these push lines.
+			instance.push(1);
+			instance.push(1);
+			instance.push(1);
+		} catch (FullStackException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(3, instance.size());
+	}
+		
+	@Test
+	public void testEmpty() {
+		System.out.println("testEmpty");
+		Stack instance = new Stack();
+		assertEquals(true, instance.empty());
+	}
+	
+	@Test
+	public void testNotEmpty() {
+		System.out.println("testNotEmpty");
+		Stack instance = new Stack();
+		try { 
+			instance.push(1);
+		} catch (FullStackException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(false, instance.empty());
+	}
 
 	@Test
 	public void testPop() {
-		fail("Not yet implemented");
-	}
+		System.out.println("testPop");
 
+		Stack instance = new Stack();
+		try {
+			// To test failure, comment out one of these push lines.
+			instance.push(1);
+			instance.push(1);
+			instance.push(1);
+			instance.pop();
+		} catch (FullStackException|EmptyStackException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(2, instance.size());
+	}
+	
 	@Test
 	public void testPush() {
-		fail("Not yet implemented");
+		System.out.println("testPush");
+		Stack instance = new Stack();
+		try {
+			instance.push(999);
+			int pushedValue = instance.peek();
+			assertEquals(999, pushedValue);
+		} catch (EmptyStackException|FullStackException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
+	
 	@Test
 	public void testPeek() {
-		fail("Not yet implemented");
+		System.out.println("testPeek");
+		int valueIAmPushing=42;
+		Stack instance = new Stack();
+		try {
+			instance.push(valueIAmPushing);
+			int returnedByPeek = instance.peek();
+			assertEquals(42, returnedByPeek);
+		} catch (EmptyStackException|FullStackException e) {
+			System.out.println(e.getMessage());
+		}
 	}
-
-	@Test
-	public void testEmpty() {
-		fail("Not yet implemented");
-	}
-	*/
-
-/* test was here */
-
-	/*
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-	*/
-
 }
